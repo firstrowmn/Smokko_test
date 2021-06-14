6a2ea7d87f83e37be8eaaaa971dc7c87ef2c7cd3
firstrowmn <80193132+firstrowmn@users.noreply.github.com>
2021-06-14 18:03:10 +0700

    main map + reviews

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+$(document).ready(function(){
 
-  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-  $("#navbutton").blur(function (event) {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
-      $("#collapsable-nav").collapse('hide');
-    }
-  });
+ h = $('#reviews').height();
+ $('#map').height(h);
+ console.log (h);
 
-  // In Firefox and Safari, the click event doesn't retain the focus
-  // on the clicked button. Therefore, the blur event will not fire on
-  // user clicking somewhere else in the page and the blur event handler
-  // which is set up above will not be called.
-  // Refer to issue #28 in the repo.
-  // Solution: force focus on the element that the click event fired on
-  $("#navbarNavDropdown").click(function (event) {
-    $(event.target).focus();
-  });
 });
